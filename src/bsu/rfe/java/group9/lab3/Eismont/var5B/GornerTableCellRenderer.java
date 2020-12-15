a905915cfe756bd9b91eb3402ccceb8d5d30b4be
Eismont Maria <75566548+Marisha-coder@users.noreply.github.com>
2020-12-15 17:49:10 +0300

    My commit

---
+++ src/bsu/rfe/java/group9/lab3/Eismont/var5B/GornerTableCellRenderer.java
@@ -0,0 +1,65 @@
+package bsu.rfe.java.group9.lab3.Eismont.var5B;
+
+import java.awt.*;
+import java.text.*;
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+
+public class GornerTableCellRenderer implements TableCellRenderer
+{
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    private String needle = null;
+    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+
+    public GornerTableCellRenderer()
+    {
+        // Показывать только 5 знаков после запятой
+        formatter.setMaximumFractionDigits(5);
+        // Не использовать группировку (т.е. не отделять тысячи ни запятыми, ни пробелами)
+        formatter.setGroupingUsed(false);
+        // Установить в качестве разделителя дробной части точку, а не  запятую
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+        // Разместить надпись внутри панели
+        panel.add(label);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col)
+    {
+        // Преобразовать double в строку с помощью форматировщика
+        String formattedDouble = formatter.format(value);
+        // Установить текст надписи равным строковому представлению числа
+        label.setText(formattedDouble);
+
+        int indexDot = formattedDouble.indexOf(".");
+        int count = 0;
+        if (indexDot != -1)
+        {
+            for (int i = indexDot + 1; i < formattedDouble.length(); i++)
+            {
+                char letter = formattedDouble.charAt(i);
+                if (letter == '1' || letter == '3' || letter == '5')
+                    count += 1;
+            }
+        }
+        if (count == formattedDouble.length() - formattedDouble.indexOf(".") - 1)
+        {
+            if (col==1 && needle!=null && needle.equals(formattedDouble))
+                panel.setBackground(Color.RED);
+            else panel.setBackground(Color.PINK);
+        }
+        else
+        {
+            if (col==1 && needle!=null && needle.equals(formattedDouble))
+                panel.setBackground(Color.RED);
+            else panel.setBackground(Color.WHITE);
+        }
+        return panel;
+    }
+    public void setNeedle(String needle)
+    {
+        this.needle = needle;
+    }
+}
---
+++ src/bsu/rfe/java/group9/lab3/Eismont/var5B/GornerTableModel.java
@@ -0,0 +1,107 @@
+package bsu.rfe.java.group9.lab3.Eismont.var5B;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GornerTableModel extends AbstractTableModel
+{
+    private Double[] coefficients;
+    private Double from;
+    private Double to;
+    private Double step;
+    private double result[] = new double[1];
+
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients)
+    {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+    public Double getFrom()
+    {
+        return from;
+    }
+    public Double getTo()
+    {
+        return to;
+    }
+    public Double getStep()
+    {
+        return step;
+    }
+    public int getColumnCount()
+    {
+        return 3;
+    }
+    public int getRowCount()
+    {
+        // Вычислить количество точек между началом и концом отрезка исходя из шага табулирования
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;
+    }
+    public Object getValueAt(int row, int col)
+    {
+        double x = from + step * row;
+        switch (col)
+        {
+            case 0:
+                return x;
+            case 1:
+            {
+                result[0] = 0.0;
+                for (int i = 0; i < coefficients.length; i++)
+                {
+                    result[0] += Math.pow(x, coefficients.length - 1 - i) * coefficients[i];
+                }
+                return result[0];
+            }
+            default:
+            {
+                result[0] = 0.0;
+                for (int i = 0; i < coefficients.length; i++)
+                    result[0] += Math.pow(x, coefficients.length - 1 - i) * coefficients[i];
+
+                int temp = 0;
+                boolean flag = true;
+                for (int j = 2; j <= result[0] - 1; j++)
+                {
+                    temp = (int)result[0] % j;
+                    if (temp == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag==true) return true;
+                else return false;
+            }
+        }
+    }
+
+    public String getColumnName(int col)
+    {
+        switch (col)
+        {
+            case 0:
+                return "Значение X";
+            case 1:
+                return "Значение многочлена";
+            default:
+                return "Значение простое?";
+        }
+    }
+    // необходимо предоставить сведения о типе данных в столбцах, в противном случае они будут отображены как строки
+    public Class<?> getColumnClass(int col)
+    {
+        switch (col)
+        {
+            case 2:
+                return Boolean.class;
+            default:
+                return Double.class;
+        }
+    }
+}
